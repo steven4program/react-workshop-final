58e4076ac8cfc775646ffeb5dd26fb7ec88d3c8c
steven4program <stevenchang421@gmail.com>
2023-08-28 16:18:58 +0800

    feature: login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './assets/scss/index.scss';
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/Login.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Check from '../assets/images/check.png';
+import Work from '../assets/images/img.png';
+
+const base_url = "https://todolist-api.hexschool.io/"
+
+function Login() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(base_url  + 'users/sign_in', formData);
+      const { token, exp } = res.data;
+      document.cookie = `hexToken=${token}; expires=${new Date(exp * 1000)}`;
+      if (res.data.status) {
+        navigate('/');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div id="loginPage" className="bg-yellow">
+      <div className="conatiner loginPage vhContainer">
+        <div className="side">
+          <a href="#" className='web-title'><img className="logoImg" src={Check} alt="checkIcon" />ONLINE TODO LIST</a>
+          <img className="d-m-n" src={Work} alt="workImg" />
+        </div>
+        <div>
+          <form className="formControls" action="index.html">
+              <h2 className="formControls_txt">最實用的線上代辦事項服務</h2>
+              <label className="formControls_label" htmlFor="email">Email</label>
+              <input className="formControls_input" type="text" id="email" name="email" placeholder="請輸入 Email" onChange={handleInputChange} required />
+              <label className="formControls_label" htmlFor="password">密碼</label>
+              <input className="formControls_input" type="password" name="password" id="password" placeholder="請輸入密碼" onChange={handleInputChange} required />
+              <input className="formControls_btnSubmit" type="button" onClick={handleSubmit} value="登入" />
+              <a className="formControls_btnLink" href="#signUpPage">註冊帳號</a>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
